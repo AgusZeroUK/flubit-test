a3cb9a7fd81b1e305415631d0c81553aa080e715
Agustin Palacios <agustinpalacios@agustins-mini.home>
2017-07-10 20:57:08 +0100

    addresses service created

---
+++ src/app/core/services/addresses/address.ts
@@ -0,0 +1,9 @@
+export interface Address {
+    title: any,
+    fullName: string,
+    line1: string,
+    line2: string,
+    county: string,
+    town: string,
+    phone: any
+}
---
+++ src/app/core/services/addresses/addresses.service.ts
@@ -0,0 +1,73 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { Http, Headers, Response } from '@angular/http';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch'
+import { Address } from './address';
+
+@Injectable()
+export class AddressesService {
+  
+  private _addresses$: BehaviorSubject<Address[]> = new BehaviorSubject([]);
+  
+  private dataStore: {
+    addresses: Address[]
+  }
+  
+
+  constructor(private http: Http) {
+      this.dataStore = {
+        addresses: []
+      }
+  
+  }
+  
+  
+  public get addresses$(){
+    return this._addresses$.asObservable();
+  }
+  
+  
+  public getAddresses(){
+        this.http.get('/api/addresses')
+            .map(this.extractData)
+            .catch(this.handleError).subscribe( addresses =>{
+                if(addresses){
+                  this.dataStore.addresses = addresses;                
+                }
+
+                this._addresses$.next(Object.assign({}, this.dataStore).addresses); 
+            });  
+
+  }
+  
+  public createAddress(Address: Address){
+    
+    
+  }
+  
+  public deleteAddress(id: number){
+    
+    
+  }
+  
+  
+  public updateAddress(address: Address){
+    
+    
+  }
+  private extractData(res: Response) {
+        const body = res.json();
+        return body;
+  }
+
+  private handleError(error: Response | any) {
+        return Observable.throw({
+            message: error._body || 'Error',
+            status: error.status
+        });
+   }
+
+}
