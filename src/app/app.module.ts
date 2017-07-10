ca7109d4ee372c6bbb6756328ebe0f8065fe6926
Agustin Palacios <agustinpalacios@agustins-mini.home>
2017-07-10 20:49:18 +0100

    core module added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { SharedModule } from './shared/shared.module';
+import { CoreModule } from './core/core.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/layout/header/header.component';
 
@@ -13,6 +14,7 @@ import { AppRoutingModule } from './app.routes';
   imports: [
     BrowserModule,
     SharedModule.forRoot(),
+    CoreModule.forRoot(),
     AppRoutingModule
   ],
   providers: [],
---
+++ src/app/core/core.module.ts
@@ -0,0 +1,33 @@
+import { NgModule, Optional, SkipSelf, ModuleWithProviders } from '@angular/core';
+
+
+
+// Services
+
+
+
+
+@NgModule({
+    imports: [
+    ],
+    exports: [
+    ],
+    providers: []
+})
+export class CoreModule {
+    // forRoot allows to override providers
+    // https://angular.io/docs/ts/latest/guide/ngmodule.html#!#core-for-root
+    public static forRoot(): ModuleWithProviders {
+        return {
+            ngModule: CoreModule,
+            providers: [
+                // Providers
+            ]
+        };
+    }
+    constructor( @Optional() @SkipSelf() parentModule: CoreModule) {
+        if (parentModule) {
+            throw new Error('CoreModule is already loaded. Import it in the AppModule only');
+        }
+    }
+}
