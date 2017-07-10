5d5e51962300d31503fa9ad5ffb287317a963b74
Agustin Palacios <agustinpalacios@agustins-mini.home>
2017-07-10 21:03:02 +0100

    service integration

--- src/app/core/services/addresses/addresses.service.ts
+++ src/app/core/services/addresses/addresses.service.ts
@@ -9,54 +9,36 @@ import { Address } from './address';
 
 @Injectable()
 export class AddressesService {
-  
   private _addresses$: BehaviorSubject<Address[]> = new BehaviorSubject([]);
-  
   private dataStore: {
     addresses: Address[]
   }
-  
 
   constructor(private http: Http) {
       this.dataStore = {
         addresses: []
       }
-  
   }
-  
-  
   public get addresses$(){
     return this._addresses$.asObservable();
   }
-  
-  
-  public getAddresses(){
+  public getAddresses() {
         this.http.get('/api/addresses')
             .map(this.extractData)
-            .catch(this.handleError).subscribe( addresses =>{
-                if(addresses){
-                  this.dataStore.addresses = addresses;                
+            .catch(this.handleError).subscribe( addresses => {
+                if (addresses) {
+                  this.dataStore.addresses = addresses;
                 }
 
-                this._addresses$.next(Object.assign({}, this.dataStore).addresses); 
-            });  
+                this._addresses$.next(Object.assign({}, this.dataStore).addresses);
+            });
 
   }
-  
-  public createAddress(Address: Address){
-    
-    
+  public createAddress(Address: Address) {
   }
-  
-  public deleteAddress(id: number){
-    
-    
+  public deleteAddress(id: number) {
   }
-  
-  
-  public updateAddress(address: Address){
-    
-    
+  public updateAddress(address: Address) {
   }
   private extractData(res: Response) {
         const body = res.json();
